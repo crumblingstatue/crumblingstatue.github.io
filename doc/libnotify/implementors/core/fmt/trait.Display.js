97a67c6d3e21935ceba24fb719aae17bea9ac902
Mika Attila <radiantstatue@gmail.com>
2015-03-22 21:34:45 +0100

    Update libnotify documentation

---
+++ doc/libnotify/implementors/core/fmt/trait.Display.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['libnotify'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Display.html' title='core::fmt::Display'>Display</a> for <a class='enum' href='libnotify/enum.ContextCreationError.html' title='libnotify::ContextCreationError'>ContextCreationError</a>","<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Display.html' title='core::fmt::Display'>Display</a> for <a class='enum' href='libnotify/enum.NotificationCreationError.html' title='libnotify::NotificationCreationError'>NotificationCreationError</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
