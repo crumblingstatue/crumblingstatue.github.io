b3f0fcfb5048502bee76432838bd1ba7df2dcff1
Mika Attila <radiantstatue@gmail.com>
2015-07-05 15:45:39 +0200

    Add try_opt documentation

---
+++ doc/try_opt/search-index.js
@@ -0,0 +1,3 @@
+var searchIndex = {};
+searchIndex['try_opt'] = {"items":[[0,"","try_opt","",null,null],[14,"try_opt!","","Helper macro for unwrapping `Option` values while returning early with an\nerror if the value of the expression is `None`. Can only be used in\nfunctions that return `Option` because of the early return of `None` that\nit provides.",null,null]],"paths":[]};
+initSearch(searchIndex);
